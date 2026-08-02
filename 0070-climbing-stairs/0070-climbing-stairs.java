e86e804da0d936dcfa6b970ea5c1ee4337c16a69
Siddhant Kumar Singh <119556802+Siddhantsingh172@users.noreply.github.com>
2026-08-02 20:55:47 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.75%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+class Solution {
+    private static HashMap<Integer, Integer> memo = new HashMap<>();
+    public int climbStairs(int n) {
+        if(n==0 || n==1) return 1;
+        if (memo.containsKey(n)) return memo.get(n);
+        int result = climbStairs(n - 1) + climbStairs(n - 2);
+        memo.put(n, result);
+
+        return result;
+    }
+}
